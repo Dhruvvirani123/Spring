0d5b2ef53bdd4cefcd4a6c0cb144df1aa43f31b5
C <viranidhruv002@gmail.com>
2023-05-03 16:01:09 +0530

    Spring Work Done

---
+++ src/main/java/com/spring/User.java
@@ -0,0 +1,36 @@
+package com.spring;
+
+public class User {
+	private int id;
+	private String name;
+	private long contact;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", contact=" + contact + ", email=" + email + "]";
+	}
+}
